01662e48df76638817c8174214a5d8f59ff63caa
Rabiulh11 <71652295+Rabiulh11@users.noreply.github.com>
2022-12-18 10:35:05 +0000

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,33 +1,39 @@
-//Using Async ajax calls with promises
-$(document).ready(function () {
+
+$(document).ready(function () { //When the document is ready 
     $("#getJoke").click(function () {
-        // Get a random joke
+        // Get a random joke from the API
         $.ajax({
-            url: "https://api.chucknorris.io/jokes/random",
-            type: "GET",
-            dataType: "json"
-        }).then(function (data) {
-            $("#joke").html(data.value); // Set the HTML content with the data that came from the server
+            url: "https://api.chucknorris.io/jokes/random", // The URL to the API. You can get this in the API page of the API you intend to consume
+            type: "GET", // The HTTP Method, can be GET POST PUT DELETE etc
+            dataType: "json", // The type of data you expect back from the server
+            success: function (data) {
+                $("#joke").html(data.value); // Set the HTML content with the data that came from the server
+            }
         });
     });
-    $("#getSearch").click(function () {
-        var search = $("#search").val();
+    $("#getSearch").click(function () { // button click event listener 
+        var search = $("#search").val(); // get the value of the input field
         $.ajax({
-            url: "https://api.chucknorris.io/jokes/search?query=" + search,
+            url: "https://api.chucknorris.io/jokes/search?query=" + search, //Search for a joke with the search term
             type: "GET",
-            dataType: "json"
-        }).then(function (data) {
-            $("#searchJoke").html(data.result[0].value); // Set the HTML content with the data that came from the server
+            dataType: "json", // The type of data you expect back from the server
+            success: function (data) {
+                $("#searchJoke").html(data.result[0].value); // Set the HTML content with the data that came from the server
+            }
         });
     });
+
+
+
     $(document).ready(function () {
         $.ajax({
             url: "https://api.chucknorris.io/jokes/categories",
             type: "GET",
-            dataType: "json"
-        }).then(function (data) {
-            for (var i = 0; i < data.length; i++) { //Loop through the categories
-                $("#categories").append("<option value='" + data[i] + "'>" + data[i] + "</option>"); //Append the categories to the select element
+            dataType: "json",
+            success: function (data) {
+                for (var i = 0; i < data.length; i++) { //Loop through the categories
+                    $("#categories").append("<option value='" + data[i] + "'>" + data[i] + "</option>"); //Append the categories to the select element
+                }
             }
         });
     });
@@ -37,9 +43,10 @@ $(document).ready(function () {
         $.ajax({
             url: "https://api.chucknorris.io/jokes/random?category=" + category,
             type: "GET",
-            dataType: "json"
-        }).then(function (data) {
-            $("#categoryJoke").html(data.value); // Set the HTML content with the data that came from the server
+            dataType: "json",
+            success: function (data) {
+                $("#categoryJoke").html(data.value); // Set the HTML content with the data that came from the server
+            }
         });
     });
 });
