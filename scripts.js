9449a87631feaded0ee015d4df7b9286a8806434
Rabiulh11 <71652295+Rabiulh11@users.noreply.github.com>
2022-12-18 10:44:21 +0000

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,52 +1,74 @@
+// Send the Api requests via the call back functions
+send_request("https://api.chucknorris.io/jokes/random", get_joke);
 
-$(document).ready(function () { //When the document is ready 
+send_request("https://api.chucknorris.io/jokes/search?query=animal", search_joke);
+
+send_request("https://api.chucknorris.io/jokes/categories", choose_category);
+
+send_request("https://api.chucknorris.io/jokes/random?category=animal", get_category);
+
+
+function send_request(api_url, callback_func) {
+    // Create an XMLHttpRequest object
+    xhr = new XMLHttpRequest();
+
+    // Define a callback function to manipulate data
+    xhr.onreadystatechange = function() {
+        callback_func(this);
+        
+    };
+    
+    // Define request
+    xhr.open("GET", api_url, true);
+
+    //Send request
+    xhr.send();
+
+};
+
+
+//Define random joke callback function
+function get_joke(xhr) {
     $("#getJoke").click(function () {
-        // Get a random joke from the API
-        $.ajax({
-            url: "https://api.chucknorris.io/jokes/random", // The URL to the API. You can get this in the API page of the API you intend to consume
-            type: "GET", // The HTTP Method, can be GET POST PUT DELETE etc
-            dataType: "json", // The type of data you expect back from the server
-            success: function (data) {
-                $("#joke").html(data.value); // Set the HTML content with the data that came from the server
-            }
-        });
+        $("#joke").html(JSON.parse(xhr.responseText).value);
     });
-    $("#getSearch").click(function () { // button click event listener 
-        var search = $("#search").val(); // get the value of the input field
-        $.ajax({
-            url: "https://api.chucknorris.io/jokes/search?query=" + search, //Search for a joke with the search term
-            type: "GET",
-            dataType: "json", // The type of data you expect back from the server
-            success: function (data) {
-                $("#searchJoke").html(data.result[0].value); // Set the HTML content with the data that came from the server
-            }
-        });
-    });
+};
 
+// Define search call back function
+function search_joke(xhr) {
+    $("#search").change(function () {
+        $("#getSearch").click(function () {
+        
+            $("#searchjoke").html(JSON.parse(xhr.responseText).value);
+            
+        });
 
+    });
+};
 
-    $(document).ready(function () {
-        $.ajax({
-            url: "https://api.chucknorris.io/jokes/categories",
-            type: "GET",
-            dataType: "json",
-            success: function (data) {
-                for (var i = 0; i < data.length; i++) { //Loop through the categories
-                    $("#categories").append("<option value='" + data[i] + "'>" + data[i] + "</option>"); //Append the categories to the select element
-                }
-            }
+// Define category call back function
+function choose_category(xhr) {
+    var data = xhr.responseText.replace(/\[|\]/g,'').split(',');
+    
+    // Loop through the categories and append the categories to the select element
+    for (var i = 0; i < data.length; i++) {
+        $("#categories").append("<option value=" + data[i] + ">" + data[i] + "</option>");
+
+        };
+    };
+
+    //Set the category fields with category search result
+function get_category(xhr) {
+    $("#categories").change(function () {
+        $("#categories").click(function () {
+            $("#categoryJoke").html(JSON.parse(xhr.responseText).value);
+
         });
     });
-    // category on change
-    $("#categories").change(function () { //When the select element is changed run the function
-        var category = $("#categories").val();
-        $.ajax({
-            url: "https://api.chucknorris.io/jokes/random?category=" + category,
-            type: "GET",
-            dataType: "json",
-            success: function (data) {
-                $("#categoryJoke").html(data.value); // Set the HTML content with the data that came from the server
-            }
-        });
-    });
-});
+};
+
+// Set api url endpoints for searching
+function get_url(api_url, search_term, btn_id) {
+    document.getElementById(btn_id).addEventListener("click", function() {
+        api_url.searchParams.set('query', $(search_term).val());});
+    };
